3ea34e85b06ce3d906e660253eadd1e2b2f36c52
Jackie Wu <wkcn@live.cn>
2017-03-23 21:08:12 +0800

    add improved method

--- hw1/base.py
+++ hw1/base.py
@@ -10,3 +10,7 @@ def get_cnts(im, L):
             gray = im[r, c]
             cnts[gray] += 1
     return cnts
+
+def get_gray(im):
+    dim = im.astype(np.int)
+    return ((dim[:,:,0] * 299 + dim[:,:,1] * 587 + dim[:,:,2] * 114 + 500) / 1000).astype(np.uint8)
